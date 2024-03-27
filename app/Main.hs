071b47eb80ecf0b2dae0f0b0c299522288e6e2ce
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-03-27 10:05:52 -0600

    some comments, and moving dictionary to it's own dir since we will have multiple

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,23 @@ module Main where
 main :: IO ()
 -- main = putStrLn "Hello, Haskell!"
 main = do 
-  let input = "Dictionary.txt"   -- dictionary
+  let input = "Dictionaries/Dictionary.txt"   -- dictionary
   contents <- readFile input
   putStrLn $ "\n" ++ show (length (lines contents)) ++ " lines \n"
   -- print the first 10 lines
   mapM_ putStrLn $ take 10 $ lines contents
+  
+  {-
+    Function to check if a word exists in the dictionary.
+      - Takes a String as input 
+      - Takes a String for the dictionary name (since we might have multiple)
+      - Returns the String if it exists
+      - Returns null if it isn't in the dictionary
+    
+    We can probably change this to return a bool later, I think returning the word will make troubleshooting 
+    easier at first. 
+    
+    findWord <SEARCH-TERM> -> <DICTIONARY-FILE-NAME> -> <RETURN>
+  -}
+  -- findWord :: [Char] -> [Char] -> [Char]
+  -- findWord x y = undefined
