9cd670b2739d0b1e5616fb046c1520d3ff49fe97
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-04-06 20:02:37 -0600

    function outline

--- app/Dictionary.hs
+++ app/Dictionary.hs
@@ -1,14 +1,17 @@
 module Dictionary(
-    search,
-    buildDictionary
+  search,
+  buildDictionary
 ) where
 
 import Data.Map as M --need a map because we don't know how many children each node is going to have 
 import Distribution.Simple.Build (build)
 
-
 data Trie = Node (M.Map Char Trie) 
   deriving (Eq, Show)
+
+getListOfStrings :: String -> [String]
+getListOfStrings = undefined
+
 
 insert :: String -> Trie -> Trie
 insert = undefined
