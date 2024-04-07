ba09ecadea385a5a6708499a7350ce3f315d9c5b
maisydunlavy <mdunlavy@unm.edu>
2024-04-06 19:31:38 -0600

    how do you do a trie in haskell

--- app/Dictionary.hs
+++ app/Dictionary.hs
@@ -1,11 +1,21 @@
 module Dictionary(
-    wordExists,
+    search,
     buildDictionary
 ) where
 
+import Data.Map as M --need a map because we don't know how many children each node is going to have 
+import Distribution.Simple.Build (build)
 
-wordExists :: String -> String -> Bool
-wordExists =  undefined 
 
-buildDictionary :: Int -> String -> String -> String
+data Trie = Node (M.Map Char Trie) 
+  deriving (Eq, Show)
+
+insert :: String -> Trie -> Trie
+insert = undefined
+
+
+search :: String -> Bool
+search =  undefined 
+
+buildDictionary :: [String] -> Trie
 buildDictionary = undefined
