43d50052ef3e11c83d6e8f55cd701ca372ff497f
maisydunlavy <mdunlavy@unm.edu>
2024-04-06 19:18:33 -0600

    dictionary

---
+++ app/Dictionary.hs
@@ -0,0 +1,11 @@
+module Dictionary(
+    wordExists,
+    buildDictionary
+) where
+
+
+wordExists :: String -> String -> Bool
+wordExists =  undefined 
+
+buildDictionary :: Int -> String -> String -> String
+buildDictionary = undefined
