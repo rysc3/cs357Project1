99b608083f3d6e2df3dfead6407adc33c66eb66d
maisydunlavy <mdunlavy@unm.edu>
2024-04-13 16:45:11 -0600

    build dictionary

--- app/Dictionary.hs
+++ app/Dictionary.hs
@@ -17,19 +17,6 @@ data Trie = Node
     } --can be a node or empty 
   deriving (Eq, Show)
 
-
-{-
-  Input from the dictionary file is of the type:
-  ghci> take 75 contents
-"aa\r\naah\r\naahed\r\naahing\r\naahs\r\naal\r\naalii\r\naaliis..."
-
-  Map across all of the input string, and split every time we have a newline, in this case, `\r\n`
-
-  Import Data.Text to help here
-  pack to convet string to Text 
-  splitOn to split into a list of strings 
-  map unpack over list to convert back to strings
--}
 getListOfStrings :: [Char] -> [[Char]]
 getListOfStrings input = map unpack $ splitOn (pack "\r\n") (pack input)
 
@@ -46,11 +33,7 @@ contains (x:xs) trie = case M.lookup x (children trie) of
     Nothing   -> False
     Just next -> contains xs next
 
-buildDictionary :: String -> [String] -> Trie
-buildDictionary s = go 
-    where 
-        go
+buildDictionary :: String -> Trie
+buildDictionary s = foldr insert emptyTrie (getListOfStrings s)
 --string will have all words in dictionary, which we will split into a list of strings, then insert each string into the trie
 
-strToList :: String -> [String]
-strToList = undefined
