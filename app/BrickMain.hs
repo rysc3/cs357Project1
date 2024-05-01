938bc8aa906b70438130140286db707a14cdcc2b
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-05-01 00:15:58 -0600

    taking input working

--- app/BrickMain.hs
+++ app/BrickMain.hs
@@ -1,26 +1,40 @@
 module BrickMain where
 
+import Control.Monad (when)
 import Options.Applicative
 import System.Exit (exitSuccess)
-import Control.Monad (when)
-
 
 data Opts = Opts
   { printHello :: Bool
   }
 
 opts :: Parser Opts
-opts = Opts
-  <$> switch
-    (  long "hello"
-    <> short 'h'
-    <> help "Print 'Hello, World!' and exit" )
+opts =
+  Opts
+    <$> switch
+      ( long "hello"
+          <> short 'h'
+          <> help "Print 'Hello, World!' and exit"
+      )
 
 fullopts :: ParserInfo Opts
-fullopts = info (helper <*> opts)
-  (  fullDesc
-  <> header "Hello World Program" )
+fullopts =
+  info
+    (helper <*> opts)
+    ( fullDesc
+        <> header "Hello World Program"
+    )
 
 main :: IO ()
 main = do
-  putStrLn "Hello, World!" >> exitSuccess
+  putStrLn "Type something"
+  loop
+
+loop :: IO ()
+loop = do
+  input <- getLine
+  if input == "exit"
+    then exitSuccess
+    else do
+      putStrLn $ "You typed: " ++ input
+      loop
