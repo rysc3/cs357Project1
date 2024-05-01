064a6e6f3140a83e3b9d34e7623af8b5c262e1d2
maisydunlavy <mdunlavy@unm.edu>
2024-05-01 08:23:14 -0600

    GUI brick

--- app/Main.hs
+++ app/Main.hs
@@ -1,74 +1,156 @@
 module Main where
 
--- imports 
-import Score (getScoringData, getLetterScore)
-import Tests (runAllTests)
-import Dictionary (buildDictionary, contains, getListOfStrings)
-import qualified BrickMain as BM
+import Control.Monad (forM, when)
+import Data.Function (on)
+import Data.List (insertBy, sortBy)
+import Data.Maybe (catMaybes, mapMaybe)
+import Data.Time.Clock
+import Data.Time.Format
+import System.Exit (exitSuccess)
+import System.IO
+import System.Random (mkStdGen, randomRs)
 
-
+import Brick
+import Brick.Widgets.Border (border)
+import Brick.Widgets.Center (vCenter)
+import qualified Graphics.Vty as V
 
 main :: IO ()
-main = do 
-  -- Maybe we should define some global things here
-  let dictionaryInputFile = "Dictionaries/01-Dictionary.txt"   -- dictionary
-  let scoreInputFile = "Dictionaries/01-Scoring.txt"                   -- scoring     -- TODO figure out how to take user input for these
-  let wordSize = 7                            -- # of letters we give the player, we can take input and set this to what they want to play with
+main = preGameLoop
 
-  runAllTests dictionaryInputFile scoreInputFile
-  BM.run
+data AppState = AppState
+    { dictionary :: [String]
+    , scoring :: [(Char, Int)]
+    , leaderboardFile :: FilePath
+    , gameState :: GameState
+    }
 
+data GameState = PreGame | Playing | ShowingLeaderboard | ShowingSettings
 
+initialState :: IO AppState
+initialState = do
+  let dictionaryInputFile = "Dictionaries/01-Dictionary.txt"
+      scoreInputFile = "Dictionaries/01-Scoring.txt"
+      leaderboardFile = "Dictionaries/Leaderboard.csv"
+  dictionary <- loadDictionary dictionaryInputFile
+  scoring <- getScoringData scoreInputFile
+  return $ AppState dictionary scoring leaderboardFile PreGame
+  where
+    loadDictionary :: FilePath -> IO [String]
+    loadDictionary dictionaryInputFile = do
+      contents <- readFile dictionaryInputFile
+      return (lines contents)
 
-  --table in brick 
-  --first line is the letters they're played, second line is letts they've played, third line is the arrow for currently selected letter
+app :: App AppState e ()
+app = App
+    { appDraw = drawUI
+    , appChooseCursor = neverShowCursor
+    , appHandleEvent = handleEvent
+    , appStartEvent = return
+    , appAttrMap = const $ attrMap V.defAttr []
+    }
 
-  --create functin to create the state 
+drawUI :: AppState -> [Widget ()]
+drawUI st =
+    case gameState st of
+        PreGame -> drawPreGameUI
+        Playing -> drawPlayingUI st
+        ShowingLeaderboard -> drawLeaderboardUI st
+        ShowingSettings -> drawSettingsUI
+    where
+        drawPreGameUI =
+            [ border $ vCenter $ hCenter $
+                vBox [ str "Select an option:"
+                     , str "1 - Play"
+                     , str "2 - Leaderboard"
+                     , str "3 - Settings"
+                     , str "4 - Quit"
+                     ]
+            ]
 
-  
-  {-
-    Function to check if a word exists in the dictionary.
-      - Takes a String as input 
-      - Takes a String for the dictionary name (since we might have multiple)
-      - Returns the String if it exists
-      - Returns null if it isn't in the dictionary
-    
-    We can probably change this to return a bool later, I think returning the word will make troubleshooting 
-    easier at first. 
-    
-    findWord <SEARCH-TERM> -> <DICTIONARY-FILE-NAME> -> <RETURN>
+        drawPlayingUI st' =
+            [ border $ vCenter $ hCenter $ drawContent st'
+            ]
 
-    ** reference shrinkDictionary
-  -}
-  -- findWord :: [Char] -> [Char] -> [Char]
-  -- findWord x y = undefined
+        drawLeaderboardUI st' =
+            [ border $ vCenter $ hCenter $
+                vBox $ map (str . formatEntry) $ take 10 $ sortLeaderboard $ parseLeaderboard st'
+            ]
 
+        drawSettingsUI =
+            [ border $ vCenter $ hCenter $
+                vBox [ str "At some point, I'll put some settings here"
+                     , str "1 - Back"
+                     ]
+            ]
 
-  {-
-    Function to shrink the dictionary, lets just take in the wordSize and return a subdictionary that only contains 
-    1. words of <= wordSize
-    2. words that only contain the user's letters
+drawContent :: AppState -> Widget ()
+drawContent st =
+    vBox [ str "How many letters would you like to play with?"
+         , str ""
+         , str "Press Enter to confirm"
+         , str ""
+         , str $ "Randomly selected letters: " ++ randomLetters
+         ]
+    where
+        randomLetters = take (numLetters st) $ randomRs ('A', 'Z') (mkStdGen 42)
 
-    Takes in an int and [Char], and retunrs a [[Char]] with all valid words in our new subdictionary
-  -}
-  -- shrinkDictionary :: int -> [Char] -> [[Char]]
+handleEvent :: AppState -> BrickEvent () e -> EventM () (Next AppState)
+handleEvent st (VtyEvent (V.EvKey (V.KChar '1') [])) = continue $ st { gameState = Playing }
+handleEvent st (VtyEvent (V.EvKey (V.KChar '2') [])) = continue $ st { gameState = ShowingLeaderboard }
+handleEvent st (VtyEvent (V.EvKey (V.KChar '3') [])) = continue $ st { gameState = ShowingSettings }
+handleEvent st (VtyEvent (V.EvKey (V.KChar '4') [])) = liftIO $ quitGame
+handleEvent st (VtyEvent (V.EvKey V.KEnter [])) =
+    case gameState st of
+        Playing -> do
+            let totalScore = calculateTotalScore st
+            liftIO $ saveQuitGame totalScore
+            continue $ st { totalScore = totalScore }
+        _ -> continue st
+handleEvent st _ = continue st
 
-  {-
-    Calculate score function. 
-      - Takes a string as input 
-      - returns an int as output
-    
-    Here, we can just hard-code values for each letter, similar to how scrabble works, and add a multiplier for length of word
+calculateTotalScore :: AppState -> Int
+calculateTotalScore st = sum $ map snd (wordScores st)
 
-  -}
-  -- scoreWord :: [Char] -> int
-  -- scoreWord x = undefined
+parseLeaderboard :: AppState -> [(Int, String, String)]
+parseLeaderboard st =
+    let contents = readFile (leaderboardFile st)
+        parsedEntries = mapMaybe parseEntry contents
+    in sortLeaderboard parsedEntries
 
-  {-
-    Fucntion takes scoring and returns tuple (<LETTER>, <SCORE>)
-  -}
-  -- scores :: [Char] -> (Char, Int)
-  -- -- read the scoring input
-  -- contents = readFile scoring
-  -- scores xs = (head xs, read (tail xs) :: 1)
-  -- putStrLn $ show $ scores "A"
+parseEntry :: String -> Maybe (Int, String, String)
+parseEntry entry
+    | null entry = Nothing  -- Skip empty lines
+    | otherwise =
+        let parts = wordsWhen (== ',') entry
+        in case parts of
+            [scoreStr, date, name] -> Just (read scoreStr, date, name)
+            _ -> Nothing
+
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s = case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
+
+formatEntry :: (Int, String, String) -> String
+formatEntry (score, date, name) = show score ++ ", " ++ date ++ ", " ++ name
+
+sortLeaderboard :: [(Int, String, String)] -> [(Int, String, String)]
+sortLeaderboard = sortBy (flip compare `on` (\(score, _, _) -> score))
+
+quitGame :: IO ()
+quitGame = do
+  putStrLn "Quitting the game..."
+  exitSuccess
+
+saveQuitGame :: Int -> IO ()
+saveQuitGame totalScore = do
+  putStrLn "------------------"
+  putStrLn "Quitting the game..."
+  putStrLn "Please enter your name:"
+  putStrLn "---"
+  name <- getLine
+  putStrLn "---"
+  currentTime <- getCurrentTime
+  let formattedDate = formatTime defaultTimeLocale
