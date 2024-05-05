a543d49b5645d6372ae0cd473c67427b138e093b
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-05-04 20:46:08 -0600

    working

--- app/Dictionary.hs
+++ app/Dictionary.hs
@@ -6,47 +6,44 @@ module Dictionary(
 ) where
 
 import qualified Data.Map.Strict as M --need a map because we don't know how many children each node is going to have
-import Data.Text (pack, splitOn, unpack, toUpper)
+import Data.Text (pack, unpack, splitOn)
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T
 
-data Trie = Node
-  { endOfWord :: Bool,
-    children :: M.Map Char Trie
-  }
-  deriving (Eq)
+-- maisy was here
 
--- Make tree show pretty
-instance Show Trie where
-  show trie = unlines $ map (showPath "") (M.toList (children trie))
-    where
-      showPath prefix (char, child) =
-        let newPath = prefix ++ [char]
-            indentation = if null prefix then "" else "    "
-            endMarker = if endOfWord child then " -> (END)" else ""
-        in indentation ++ newPath ++ endMarker ++ "\n" ++ showPath' newPath child
-      showPath' prefix (Node _ children') = unlines $ map (showPath prefix) (M.toList children')
+data Trie = Node {
+  endOfWord :: Bool,
+  children :: M.Map Char Trie
+}
+  deriving (Eq, Show)
 
+{-
+  Input from the dictionary file is of the type:
+  ghci> take 75 contents
+"aa\r\naah\r\naahed\r\naahing\r\naahs\r\naal\r\naalii\r\naaliis..."
+
+  Map across all of the input string, and split every time we have a newline, in this case, `\r\n`
+
+  Import Data.Text to help here
+  pack to convet string to Text 
+  splitOn to split into a list of strings 
+  map unpack over list to convert back to strings
+-}
 emptyTrie :: Trie
 emptyTrie = Node False M.empty
 
-{-
-  TODO @here 
-  We need to convert every letter to be CAPITALIZED as we add it to the tree. Right now contains is case sensitive and returns false for a word if it has any capitalizations. 
-    
-    In the scoring data I have everything working by converting to be capitalized firsrt, so we should do the same thing here to keep it all uniform.
--}
 insert :: String -> Trie -> Trie
-insert [] trie = trie {endOfWord = True}
-insert (x : xs) trie =
-  let childNode = M.lookup x (children trie)
-      newNode = insert xs (maybe emptyTrie id childNode)
-      updatedChildren = M.insert x newNode (children trie)
-   in trie {children = updatedChildren}
+insert []     trie = trie { endOfWord = True }
+insert (x:xs) trie =
+    let childNode = M.lookup x (children trie)
+        newNode = insert xs (maybe emptyTrie id childNode)
+        updatedChildren = M.insert x newNode (children trie)
+    in trie { children = updatedChildren }
 
 contains :: String -> Trie -> Bool
 contains [] trie = endOfWord trie
-contains (x : xs) trie = maybe False (contains xs) (M.lookup x (children trie))
+contains (x:xs) trie = maybe False (contains xs) (M.lookup x (children trie)) --need to handle for nothing or Just cases when called 
 
 buildDictionary :: FilePath -> IO Trie
 buildDictionary filePath = do
